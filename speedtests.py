b09d62aa613aa2367a535984dcd090c4d365e992
Mark Cote <mcote@mozilla.com>
2011-03-22 17:08:44 -0400

    Increased fish number to 1000. Refactoring of client script. Replace profiles if local archives exist.

--- speedtests.py
+++ speedtests.py
@@ -5,12 +5,14 @@ import ConfigParser
 import errno
 import os
 import platform
+import shutil
 import socket
 import subprocess
 import sys
 import tempfile
 import threading
 import time
+import zipfile
 
 DEFAULT_CONF_FILE = 'speedtests.conf'
 cfg = ConfigParser.ConfigParser()
@@ -30,74 +32,62 @@ class TestsFinishedException(Exception):
 
 class BrowserLauncher(object):
     
-    def __init__(self, cmd_tuple):
-        self.cmd_tuple = cmd_tuple
+    def __init__(self, os_name, browser_name, profile_path, cmd, args_tuple=()):
+        self.os_name = os_name
+        self.browser_name = browser_name
+        self.profile_path = profile_path
+        self.cmd = cmd
+        self.args_tuple = args_tuple
+        self.proc = None
+        self.previous_profile = ''
+        try:
+            self.cmd = cfg.get(os_name, browser_name)
+        except (ConfigParser.NoSectionError, ConfigParser.NoOptionError):
+            pass
 
     def cmd_line(self, url=TEST_URL):
-        return self.cmd_tuple + (url,)
+        return (self.cmd,) + self.args_tuple + (url,)
 
     def browser_exists(self):
-        return os.path.exists(self.cmd_tuple[0])
-
-
-class BrowserLauncherRedirFile(BrowserLauncher):
+        return os.path.exists(self.cmd)
     
-    def __init__(self, cmd_tuple):
-        self.cmd_tuple = cmd_tuple
-        self.redir_file = None
-    
-    def cmd_line(self, url=TEST_URL):
-        self.redir_file = tempfile.NamedTemporaryFile(suffix='.html')
-        self.redir_file.write('<html><head><meta HTTP-EQUIV="REFRESH" content="0; url=%s"></head></html>\n' % url)
-        self.redir_file.flush()
-        return super(BrowserLauncherRedirFile, self).cmd_line(self.redir_file.name)
-        
-
-class BrowserRunner(object):
-
-    @classmethod
-    def browsers_by_os(cls, os_str):
-        if os_str == 'Darwin':
-            return [
-                   BrowserLauncher(('/Applications/Firefox.app/Contents/MacOS/firefox', '-private')),
-                   BrowserLauncherRedirFile(('/Applications/Safari.app/Contents/MacOS/Safari',)),
-                   BrowserLauncher(('/Applications/Opera.app/Contents/MacOS/Opera',)),
-                   BrowserLauncher(('/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',)),
-                   ]
-        elif os_str == 'Linux':
-            return []
-        elif os_str == 'Windows':
-            return [
-                   BrowserLauncher(('\\Program Files\\Mozilla Firefox 4.0 Beta 12\\firefox.exe', '-private')),
-                   BrowserLauncher(('\\Program Files\\Internet Explorer\\iexplore.exe',)),
-                   BrowserLauncher(('\\Program Files\\Safari\\Safari.exe',)),
-                   BrowserLauncher(('\\Program Files\\Opera\\opera.exe',)),
-                   BrowserLauncher((os.path.join(os.getenv('USERPROFILE'), 'Local Settings\\Application Data\\Google\\Chrome\\Application\\chrome.exe'),)),
-                   ]
-
-
-    def __init__(self, evt):
-        self.evt = evt
+    def copy_profile(self):
+        if not self.browser_exists():
+            return
+        profile_archive = 'profile_%s_%s.zip' % (self.browser_name, self.os_name)
+        if not os.path.exists(profile_archive):
+            return
+        if os.path.exists(self.profile_path):
+            t = tempfile.mkdtemp()
+            shutil.move(self.profile_path, t)
+            self.previous_profile = os.path.join(t, os.path.basename(self.profile_path))
         try:
-            self.browsers = BrowserRunner.browsers_by_os(platform.system())
-        except KeyError:
-            sys.stderr.write('Unknown platform "%s".\n' % platform.system())
-            sys.exit(errno.EOPNOTSUPP)
-        self.browser_iter = iter(self.browsers)
-        self.current_launcher = None
-        self.proc = None
-        self.lock = threading.Lock()
+            os.mkdir(self.profile_path)
+        except OSError:
+            pass
+        profile_zip = zipfile.ZipFile(profile_archive)
+        profile_zip.extractall(self.profile_path)
     
-    def browser_running(self):
-        self.lock.acquire()
+    def clean_up(self):
+        if not self.previous_profile:
+            return
+        shutil.rmtree(self.profile_path)
+        shutil.move(self.previous_profile, self.profile_path)
+        os.rmdir(os.path.dirname(self.previous_profile))
+
+    def launch(self):
+        self.copy_profile()
+        cl = self.cmd_line()
+        print 'Launching %s...' % ' '.join(cl)
+        self.proc = subprocess.Popen(cl)
+
+    def running(self):
         running = self.proc and self.proc.poll()
         if running != None:
             self.proc = None
-        self.lock.release()
         return running == None
 
-    def launch_next_browser(self):
-        self.lock.acquire()
+    def terminate(self):
         if self.proc:
             print 'terminating process'
             self.proc.terminate()
@@ -114,6 +104,74 @@ class BrowserRunner(object):
                 self.proc.wait()  # or poll and error out if still running?
                 self.proc = None
             print 'process is dead'
+        self.clean_up()
+
+
+
+class BrowserLauncherRedirFile(BrowserLauncher):
+    
+    def __init__(self, os_name, browser_name, profile_path, cmd, args_tuple=()):
+        super(BrowserLauncherRedirFile, self).__init__(self, os_name, browser_name, profile_path, cmd, args_tuple)
+        self.redir_file = None
+    
+    def cmd_line(self, url=TEST_URL):
+        self.redir_file = tempfile.NamedTemporaryFile(suffix='.html')
+        self.redir_file.write('<html><head><meta HTTP-EQUIV="REFRESH" content="0; url=%s"></head></html>\n' % url)
+        self.redir_file.flush()
+        return super(BrowserLauncherRedirFile, self).cmd_line(self.redir_file.name)
+        
+
+class BrowserRunner(object):
+
+    @classmethod
+    def browsers_by_os(cls, os_str):
+        if os_str == 'Darwin':
+            os_name = 'osx'
+            return [
+                   BrowserLauncher(os_name, 'firefox', '/Applications/Firefox.app/Contents/MacOS/firefox'),
+                   BrowserLauncherRedirFile(os_name, 'safari', '/Applications/Safari.app/Contents/MacOS/Safari'),
+                   BrowserLauncher(os_name, 'opera', '/Applications/Opera.app/Contents/MacOS/Opera'),
+                   BrowserLauncher(os_name, 'chrome', '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome')
+                   ]
+        elif os_str == 'Linux':
+            os_name = 'linux'
+            return [
+                   BrowserLauncher(os_name, 'firefox', os.path.join(os.getenv('HOME'), '.mozilla', 'firefox'), '/usr/bin/firefox'),
+                   BrowserLauncher(os_name, 'opera', os.path.join(os.getenv('HOME'), '.opera'), '/usr/bin/opera'),
+                   BrowserLauncher(os_name, 'chrome', os.path.join(os.getenv('HOME'), '.config', 'google-chrome'), '/usr/bin/google-chrome')
+                   ]
+        elif os_str == 'Windows':
+            os_name = 'windows'
+            return [
+                   BrowserLauncher(os_name, 'firefox', '\\Program Files\\Mozilla Firefox 4.0 Beta 12\\firefox.exe'),
+                   BrowserLauncher(os_name, 'internet explorer', '\\Program Files\\Internet Explorer\\iexplore.exe'),
+                   BrowserLauncher(os_name, 'safari', '\\Program Files\\Safari\\Safari.exe'),
+                   BrowserLauncher(os_name, 'opera', '\\Program Files\\Opera\\opera.exe'),
+                   BrowserLauncher(os_name, 'chrome', os.path.join(os.getenv('USERPROFILE'), 'Local Settings\\Application Data\\Google\\Chrome\\Application\\chrome.exe'))
+                   ]
+
+    def __init__(self, evt):
+        self.evt = evt
+        try:
+            self.browsers = BrowserRunner.browsers_by_os(platform.system())
+        except KeyError:
+            sys.stderr.write('Unknown platform "%s".\n' % platform.system())
+            sys.exit(errno.EOPNOTSUPP)
+        self.browser_iter = iter(self.browsers)
+        self.current_launcher = None
+        self.proc = None
+        self.lock = threading.Lock()
+    
+    def browser_running(self):
+        self.lock.acquire()
+        running = self.current_launcher.running()
+        self.lock.release()
+        return running
+
+    def launch_next_browser(self):
+        self.lock.acquire()
+        if self.current_launcher:
+            self.current_launcher.terminate()
 
         while True:
             try:
@@ -125,9 +183,7 @@ class BrowserRunner(object):
             if self.current_launcher.browser_exists():
                 break
 
-        cl = self.current_launcher.cmd_line()
-        print 'Launching %s...' % ' '.join(cl)
-        self.proc = subprocess.Popen(cl)
+        self.proc = self.current_launcher.launch()
         self.lock.release()
 
 
