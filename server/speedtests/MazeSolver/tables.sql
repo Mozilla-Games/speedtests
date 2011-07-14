7c59c63b2c21ccf59171447e5651e52449fde06b
Mark Cote <mcote@mozilla.com>
2011-07-14 09:41:25 -0700

    Added schema for MazeSolver.

---
+++ server/speedtests/MazeSolver/tables.sql
@@ -0,0 +1,11 @@
+CREATE TABLE MazeSolver (
+    id INT AUTO_INCREMENT,
+    PRIMARY KEY (id),
+    browser_id INT,
+    FOREIGN KEY (browser_id) REFERENCES browser(id),
+    browser_height INT,
+    browser_width INT,
+    score INT,
+    teststart TIMESTAMP,
+    ip VARCHAR(15) COLLATE utf8_bin
+);
