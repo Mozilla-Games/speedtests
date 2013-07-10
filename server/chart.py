8b9167c51a50e23c3ea265bc4a0f54d47d527299
Alan Kligman <ack@mozilla.com>
2013-07-10 09:53:14 -0400

    Update chart format.

--- server/chart.py
+++ server/chart.py
@@ -3,17 +3,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-benchmark = 'aquarium'
-# browsers = ['firefox', 'firefox-nightly', 'chrome', 'chrome-beta']
+#browsers = ['firefox', 'firefox-nightly', 'chrome', 'chrome-beta']
+#scores = {
+#  'aquarium': [21, 33, 27, 25],
+#  'octane': [10, 11, 8, 15],
+#  'sunspider-1.0': [32, 22, 14, 23]
+#}
+benchmarks = ['aquarium', 'octane', 'sunspider-1.0']
 scores = {
-  'firefox': 21,
-  'nigtly': 33,
-  'chrome': 27,
-  'chrome-beta': 25,
+  'firefox': [21, 10, 32],
+  'nightly': [33, 11, 22]
 }
-browsers = scores.keys();
+browsers = scores.keys()
 
-N = len(browsers);
+N = len(benchmarks);
 
 ind = np.arange(N)  # the x locations for the groups
 width = 0.2        # the width of the bars
@@ -21,21 +24,22 @@ width = 0.2        # the width of the bars
 fig = plt.figure()
 ax = fig.add_subplot(111)
 
+rects = dict.fromkeys(scores);
 offset = 0;
 cm = plt.get_cmap('gist_rainbow')
 ax.set_color_cycle([cm(1.*i/N) for i in range(N)])
-
-this_color = color = cm(1.*offset/N)
-rects = ax.bar(ind+width*offset, scores.values(), width, color=this_color)
-offset += 1
+for browser in rects.keys():
+  this_color = color = cm(1.*offset/N)
+  rects[browser] = ax.bar(ind+width*offset, scores[browser], width, color=this_color)
+  offset += 1
 
 # add some
 ax.set_ylabel('scores')
-ax.set_title(benchmark)
+ax.set_title('benchmarks')
 ax.set_xticks(ind+width)
-ax.set_xticklabels( browsers )
+ax.set_xticklabels( benchmarks )
 
-ax.legend( [rects[0]], [benchmark] )
+ax.legend( [rect[0] for rect in rects.values()], browsers )
 
 def autolabel(rects):
     # attach some text labels
@@ -44,6 +48,7 @@ def autolabel(rects):
         ax.text(rect.get_x()+rect.get_width()/2., 1.05*height, '%d'%int(height),
                 ha='center', va='bottom')
 
-autolabel(rects)
+for rect in rects.values():
+  autolabel(rect)
 
 plt.show()
