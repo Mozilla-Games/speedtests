0980d57db4cd1f37de18ae88497567f051cbeee2
Alan Kligman <ack@mozilla.com>
2013-07-16 14:34:08 -0400

    Pull values from database and compute averages.

--- server/chart.py
+++ server/chart.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python
 
+import sys
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 import ConfigParser
 import web
+
+def pretty(d, indent=0):
+  for key, value in d.iteritems():
+    print '\t' * indent + str(key)
+    if isinstance(value, dict):
+       pretty(value, indent+1)
+    else:
+       print '\t' * (indent+1) + str(value)
 
 class DefaultConfigParser(ConfigParser.ConfigParser):
 
@@ -49,8 +59,84 @@ except:
     raise
 
 # TODO: load these from options, scores from db
-platform = 'android'
-benchmarks = ['webgl-aquarium', 'octane', 'sunspider']
+platform = 'Android'
+client = 'GalaxyNexus'
+benchmarks = ['octane']
+browser_ids = [5, 6]
+
+benchmark_data = {}
+browser_data = {}
+
+for benchmark in benchmarks:
+  for bid in browser_ids:
+    benchmark_data[bid] = {}
+    vars = {
+      'i': bid
+    }
+    entries = db.select(['browsers'], vars, what='name, version, platform', where='id=$i')
+    try:
+      browser_data[bid] = dict(entries[0])
+    except:
+      print "Browser ID not found"
+      raise
+
+    result = None;
+
+    vars = {
+      'c': client,
+      'b': benchmark,
+      'i': bid
+    }
+    entries = db.select(['runs'], vars, what='uuid', where='client=$c AND bench_name=$b AND browser_id=$i', order='start_time desc', limit=1)
+    try:
+      result = dict(entries[0])
+    except:
+      print "No entries found!"
+      raise
+
+    vars = {
+      'u': result['uuid']
+    }
+    iterations = db.select(['iterations'], vars, what='id', where='run_uuid=$u')
+    result['scores'] = {}
+    for i in iterations:
+      iid = i['id']
+
+      vars = {
+        'i': iid
+      }
+      scores = db.select(['scores'], vars, what='id, score, test_name, window_width, window_height, extra_data', where='iteration_id=$i')
+      for score in scores:
+        sname = score['test_name']
+        sid = score['id']
+        if not sname in result['scores']:
+          result['scores'][sname] = {}
+        result['scores'][sname][sid] = dict(score)
+
+    benchmark_data[bid][benchmark] = result
+
+print pretty(benchmark_data)
+print pretty(browser_data)
+
+processed_benchmark_data = {}
+for bid in benchmark_data.keys():
+  processed_benchmark_data[bid] = {}
+  for bench in benchmark_data[bid].keys():
+    processed_benchmark_data[bid][bench] = {}
+    tests = benchmark_data[bid][bench]['scores']
+    for test_name, test_results in tests.items():
+      processed_benchmark_data[bid][bench][test_name] = {}
+      sum_scores = 0
+      for result in test_results.values():
+        sum_scores += result['score']
+      avg_score = sum_scores / len(test_results)
+      processed_benchmark_data[bid][bench][test_name]['score'] = avg_score
+      # TODO: compute error, if possible
+
+print pretty(processed_benchmark_data)
+
+sys.exit(0);
+
 scores = {
   'firefox': [21, 10, 32],
   'nightly': [33, 11, 22]
@@ -93,4 +179,4 @@ def autolabel(rects):
 for rect in rects.values():
   autolabel(rect)
 
-plt.show()
+# plt.show()
